d10edb3c29e23674ca956dd15c609247fa376fc1
johnrraymond <johnrraymond@yahoo.com>
2023-01-24 14:42:11 -0800

    add images and bios
    Not sure if i busted stuff but we need to get this page filled in so i did it. :)

--- src/components/Team.js
+++ src/components/Team.js
@@ -74,7 +74,62 @@ const Team = () => {
                         <p className="team-title">Team</p>
                     </div>
                 </div>
+
                 <div className="row team-member-area">
+
+                    <div className="col-md-6">
+                        <div className="team-member-item" data-aos="fade-right">
+                            <div className="team-member-avatar">
+                                <img src="/img/jp-medium.png" width="300" alt="team member avatar"></img>
+                            </div>
+                            <h3 className="member-title">Jason Palmer</h3>
+                            <p className="member-description">Jason Palmer – Chief Executive Officer: An MBA graduate, and a former musician, Jason has a deep understanding of business process and negotiation. His background is in managing a portfolio of vendors and controlling overall business spend, while ensuring vendors are paid promptly and agreed upon contractual obligations are met. A hands-off leader, Jason thrives when he ensures his best folks have the freedom to do their best work.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="team-member-item" data-aos="fade-right">
+                            <div className="team-member-avatar">
+                                <img src="/img/kn-medium.png" width="300" alt="team member avatar"></img>
+                            </div>
+                            <h3 className="member-title">Kazuya Nomura</h3>
+                            <p className="member-description">Kazuya Nomura – Lead Developer: Kazuya has worked in several kinds of blockchain applications for 5 years and web apps for 8 years. As a creative & passionate developer or a lead developer, and fast learner with details and high quality, he is always trying to improve development and team communication processes.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="team-member-item" data-aos="fade-right">
+                            <div className="team-member-avatar">
+                                <img src="/img/lk-medium.png" width="300" alt="team member avatar"></img>
+                            </div>
+                            <h3 className="member-title">Leon Kanei</h3>
+                            <p className="member-description">Leon Kanei is a Full-Stack Web Developer at The Great Library. With over 5 years of experience in creating cohesive and engaging experiences for clients, Leon is skilled in Javascript/Typescript, Web3/Ethers, Solidity, React.js, and more. He has worked on a range of projects, including decentralized yield farming protocols, NFT yield farming platforms, and financial data companies. In his free time, Leon enjoys gaming, photography, climbing, and camping.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="team-member-item" data-aos="fade-right">
+                            <div className="team-member-avatar">
+                                <img src="/img/cb-medium.png" width="300" alt="team member avatar"></img>
+                            </div>
+                            <h3 className="member-title">Collin Brown</h3>
+                            <p className="member-description">Collin Brown – Lead Unity Developer: With over 8 years of cooperative and independent development experience, Collin has developed many complex games, a variety of applications, platforms, and software titles. His background covers a wide variety of software technologies, not the least of which are Blockchain and Unity Development. He has worked as a Senior Game and Blockchain Developer for Zomfi, Crypto Racing, and MarketMaking Pro. His personal projects include Age of Kingdom, a battle strategy game where users can play with millions of online players from all over the world, and Critical Ops, a 3D Multiplayer FPS game which features competitive combat, beautiful maps, and several game modes.  </p>
+                        </div>
+                    </div>
+
+
+                    <div className="col-md-6">
+                        <div className="team-member-item" data-aos="fade-right">
+                            <div className="team-member-avatar">
+                                <img src="/img/ms-medium.png" width="300" alt="team member avatar"></img>
+                            </div>
+                            <h3 className="member-title">Magic Smart</h3>
+                            <p className="member-description">Magic Smart – Smart Contract Developer: Magic is a senior blockchain engineer, with many years of experience and talent skill. He was born in Japan. He has 6+ years of experience in web development and 5+ years of experience in Blockchain development giving his deep knowledge about the web and smart contracts. After graduation, he entered several regular companies in Japan & Canada. He has worked on Flow, Algorand, Ethereum, CosWasm, Substrate, Polkadot and Solana technology. In 2017, he focused on blockchain technology and started to research it.</p>
+                        </div>
+                    </div>
+
+	            <p className="team-title">Authors</p>
+
                     <div className="col-md-6">
                         <div className="team-member-item" data-aos="fade-right">
                             <div className="team-member-avatar">
@@ -84,6 +139,7 @@ const Team = () => {
                             <p className="member-description">John R Raymond is the author of the Scarab Cycle. His background in Byzantine Agreement and cryptography lead him to create The Great Library when it became clear to him that the publishing industry was ripe for a shake up. He is currently the Head Librarian with The Great Library and, when not busy with his own books, he works with others editing theirs. He likes both cats and dogs and supports the arts in whatever form they take.</p>
                         </div>
                     </div>
+
                     <div className="col-md-6">
                         <div className="team-member-item" data-aos="fade-left">
                             <div className="team-member-avatar">
